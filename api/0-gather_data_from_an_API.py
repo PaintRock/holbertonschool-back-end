69c43e33901d1b87f630ba74945168be72450729
PaintRock <6126@holbertonstudents.com>
2023-07-22 20:41:24 +0000

    whynot

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,26 +6,28 @@ import sys
 
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    todos_url = ("https://jsonplaceholder.typicode.com/users/todos")
 
-    # Fetching employee details
+    """ Fetching employee details """
     employee_response = requests.get(f"{base_url}/{employee_id}")
     employee_data = employee_response.json()
     employee_name = employee_data['name']
 
-    # Fetching TODO list for the employee
-    todos_response = requests.get(todos_url)
+    """ Fetching TODO list for the employee """
+    todos_response = requests.get(todos_url, params={"userId": employee_id})
     todos_data = todos_response.json()
 
-    # Counting completed tasks and getting total tasks count
+    """ Counting completed tasks and getting total tasks count """
     total_tasks = len(todos_data)
     completed_tasks = sum(1 for todo in todos_data if todo['completed'])
 
     # Displaying results
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks"
+          f"({completed_tasks}/{total_tasks}):")
     for todo in todos_data:
         if todo['completed']:
-            print("    ", todo['title'])  # Use four spaces for indentation
+            print("\t", todo['title'])  # Use four spaces for indentation
+
 
 if __name__ == "__main__":
     try:
