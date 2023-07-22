7dd145e5aa20d92dce949642c406151292d85f06
PaintRock <6126@holbertonstudents.com>
2023-07-22 01:52:52 +0000

    0v1

---
+++ 0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" API tool to retrive datat from an URL """
+import requests
+
+
+def get_employee_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos/1"
+
+    # Fetching employee details
+    employee_response = requests.get(f"{base_url}/{employee_id}")
+    employee_data = employee_response.json()
+    employee_name = employee_data['name']
+
+    # Fetching TODO list for the employee
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    # Counting completed tasks and getting total tasks count
+    total_tasks = len(todos_data)
+    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+
+    # Displaying results
+    print(f"Employee {employee_name}\
+        is done with tasks({completed_tasks}/{total_tasks}):")
+    for todo in todos_data:
+        if todo['completed']:
+            print("\t", todo['title'])
+
+
+if __name__ == "__main__":
+    try:
+        employee_id = int(input("Enter the employee ID: "))
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Invalid input. Please enter an integer for the employee ID.")
+
+
+py
