3b80aac809b19c4aec10f2ac1ce01ba35ea997d0
PaintRock <6126@holbertonstudents.com>
2023-07-22 22:19:46 +0000

    meandcrystal

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -44,7 +44,8 @@ def export_to_csv(employee_name, completed_tasks, total_tasks, tasks_list):
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
         for task_title in tasks_list:
-            csv_writer.writerow([argv[1], employee_name, "True" if task_title in completed_tasks else "False", task_title])
+            csv_writer.writerow([argv[1], employee_name,
+                                 "True" if task_title in completed_tasks else "False", task_title])
 
 
 if __name__ == '__main__':
