9cf5e2f9a59a4729bac9f6677318ee093b518994
PaintRock <6126@holbertonstudents.com>
2023-07-23 02:07:09 +0000

    id to user_id

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -27,9 +27,9 @@ def gather():
     user_id = user_json[0]['id']
     tasks_list = []
     list_date = []
-    
+
     for date in all_json:
-        info = [str(id), name, str(date['completed']), date['title']]
+        info = [str(user_id), name, str(date['completed']), date['title']]
         list_date.append(info)
 
     for task in all_json:
